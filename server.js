84eef990f9748dd4f2918e48cb5fc739582209f9
zxcv51523 <74313695+zxcv51523@users.noreply.github.com>
2021-10-26 15:32:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mineflayer = require('mineflayer')
 
 const bot = mineflayer.createBot({
   host: 'MarssSMP.aternos.me', // minecraft server ip
-  username: '24_7bot', // minecraft username
+  username: 'krompir_alt', // minecraft username
   // password: '' // minecraft password, comment out if you want to log into online-mode=false servers
   port: 31444,                // only set if you need a port that isn't 25565
   // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
